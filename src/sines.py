00ffed159785a7407d996a6f727bd25d8d0ac553
Chad Estioco <chadestioco@gmail.com>
2025-05-27 18:50:13 +0200

    POC: This library as a crude TUI.
    You can now scroll through the sines demo.

--- src/sines.py
+++ src/sines.py
@@ -6,15 +6,17 @@ __VERSION__ = "0.1.0"
 
 class Sine(Signal):
 
-    def __init__(self, y_lattice_length=4, x_lattice_length=4, max_x=8):
+    def __init__(self, y_lattice_length=4, x_lattice_length=4, max_x=8, wait_input=False):
         # How many rows until we get one lattick unit of the y-axis
         self.y_lattice_length = y_lattice_length
         # How many characters until we get one lattick unit of the x-axis
         self.x_lattice_length = x_lattice_length
         self.max_x = max_x
+        self.wait_input = wait_input
 
         self.frame_size = max_x * x_lattice_length
         self.window = []
+        self.tick = 0
 
     def __draw_line(self, lineno):
         """
@@ -59,15 +61,22 @@ class Sine(Signal):
         bufr.append("Tick: " + str(self.tick))
         return "\n".join(bufr)
     
-    def __make_noise(self, tick):
-        self.tick = tick
-        self.phase_shift = (math.pi / self.x_lattice_length) * tick
+    def __make_noise(self):
+        self.phase_shift = (math.pi / self.x_lattice_length) * self.tick
         self.window = [math.sin(((ix * math.pi) / self.x_lattice_length) + self.phase_shift) for ix in range(self.frame_size)]
         return self.draw_graph()
 
     def generate(self, ticks):
-        for t in range(ticks):
-            yield self.__make_noise(t)
+        while self.tick < ticks:
+            yield self.__make_noise()
+            if self.wait_input:
+                cmd = input("scroll: ").lower()
+                if cmd == "j":
+                    self.tick += 1
+                elif cmd == "k":
+                    self.tick -= 1
+            else:
+                self.tick += 1
 
 if __name__ == "__main__":
-    Scope(Sine()).flow()
+    Scope(Sine(wait_input=True)).flow()
